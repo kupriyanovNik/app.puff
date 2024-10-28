27fed069082acf5a27287ab33eee1601bdc89dd1
kupriyanovNik <cucuprianov@icloud.com>
2024-10-28 21:08:12 +0300

    fixed weekly chart

--- Puff/Puff/Presentation/Modules/Statistics/Components/Weekly/StatisticsWeeklyChart.swift
+++ Puff/Puff/Presentation/Modules/Statistics/Components/Weekly/StatisticsWeeklyChart.swift
@@ -133,7 +133,7 @@ struct StatisticsWeeklyChart: View {
         VStack(spacing: 8) {
             HStack(spacing: 4) {
                 let symbols: [String] = {
-                    var weekdaySymbols = Calendar(identifier: .iso8601).shortWeekdaySymbols
+                    var weekdaySymbols = Calendar.current.shortWeekdaySymbols
                     weekdaySymbols.append(weekdaySymbols.remove(at: weekdaySymbols.startIndex))
 
                     return weekdaySymbols.map { $0.prefix(2).capitalized }
